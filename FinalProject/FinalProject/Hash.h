f27bc099f523474be14f442e2747417f92440c6b
peterkirov98 <peterkirov98@gmail.com>
2018-03-09 20:11:33 -0800

    new changes to hash table
    not completed yet

--- FinalProject/FinalProject/Hash.h
+++ FinalProject/FinalProject/Hash.h
@@ -1,4 +1,10 @@
 #pragma once
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 
 #ifndef Hash_h
 #define Hash_h
@@ -7,8 +13,24 @@ class Hash
 {
 private:
 
-public:
+	static const int tableSize = 69;
 
+	struct player
+	{
+		string name;
+		int matches, wins, losses, winPercent;
+		player* next;
+	};
+
+	player* hashTable[tableSize];
+
+public:
+	Hash();
+	int hash(string key);
+	void addItem(string name, int matches, int wins, int losses, int winPercent);
+	int numberOfItemsInIndex(int index);
+	void PrintTable();
+	void PrintItemsInIndex(int index);
 
 };
 
--- FinalProject/FinalProject/main.cpp
+++ FinalProject/FinalProject/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include "Hash.h"
+
+using namespace std;
+
+
 int main()
 {
 
+	return 0;
 }
