9f690d29f4216a855c831caf949274dff4ed380e
SeeLtephen <32529372+SeeLtephen@users.noreply.github.com>
2018-03-12 16:35:02 -0700

    Minor Changes

--- FinalProject/FinalProject/Hash.h
+++ FinalProject/FinalProject/Hash.h
@@ -23,7 +23,6 @@ private:
 	};
 
 	player* hashTable[tableSize];
-
 public:
 	Hash();
 	int hash(string key);
@@ -31,6 +30,29 @@ public:
 	int numberOfItemsInIndex(int index);
 	void PrintTable();
 	void PrintItemsInIndex(int index);
+
+	/*class iterator
+	{
+	private:
+		List<HashMapNode<T>*>* mapNodes;
+		int position;
+		unsigned int operations;
+	public:
+		iterator() {
+			mapNodes = nullptr;
+			position = 0;
+		}
+		iterator(List<HashMapNode<T>*>* ptr, int pos) {
+			mapNodes = ptr;
+			position = pos;
+		}
+		iterator operator++() { iterator i = *this; position++; return i; }
+		iterator operator++(int junk) { position++; return *this; }
+		HashMapNode<T>& operator*() { return *((*mapNodes)[position]); }
+		HashMapNode<T>* operator->() { return (*mapNodes)[position]; }
+		bool operator==(const iterator& rhs) { return position == rhs.position; }
+		bool operator!=(const iterator& rhs) { return position != rhs.position; }
+	};*/
 
 };
 
---
+++ FinalProject/FinalProject/LinkedList.h
@@ -0,0 +1,6 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+template <class T>
+
+
+#endif // !LINKEDLIST_H
